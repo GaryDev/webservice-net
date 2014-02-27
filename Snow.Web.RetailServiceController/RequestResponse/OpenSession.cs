85078fcfd910e8ab4f30cdae460633b94a03da2d
Gary.Guo <shane8328@gmail.com>
2014-02-27 15:23:02 +0800

    init commit for retail service

---
+++ Snow.Web.RetailServiceController/RequestResponse/OpenSession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Snow.Web.RetailService
+{
+    [DataContract(Namespace = "http://snow.org/retailservice/2014/02/27/")]
+    public class OpenSessionRequest
+    {
+        [DataMember(Name = "LoginId", Order = 0)]
+        public string LoginId { get; set; }
+        [DataMember(Name = "ComputerName", Order = 1)]
+        public string ComputerName { get; set; }
+    }
+
+    [DataContract(Namespace = "http://snow.org/retailservice/2014/02/27/")]
+    public class OpenSessionResponse
+    {
+        [DataMember(Name = "SessionId", Order = 0)]
+        public string SessionId { get; set; }
+    }
+}
---
+++ Snow.Web.RetailServiceController/RetailServiceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snow.Web.Controller
+{
+    public class RetailServiceController
+    {
+        public string SessionId { get; set; }
+        public string ClientTime { get; set; }
+
+        public RetailServiceController(string sessionId, string clientTime)
+        {
+            this.SessionId = sessionId;
+            this.ClientTime = clientTime;
+        }
+        
+        public RetailService.OpenSessionResponse OpenSession(RetailService.OpenSessionRequest request)
+        {
+            string sessionId = BLL.RetailServiceBLL.NewSession(request.LoginId, request.ComputerName);
+            return new RetailService.OpenSessionResponse() { SessionId = sessionId };
+        }
+
+    }
+}
