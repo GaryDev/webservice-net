c9f707fd4be42ee36805a07cb1b5484b44e10441
GaryDev <shane8328@gmail.com>
2015-04-23 16:27:19 +0800

    DataProvider - MySql

--- Snow.Web.DataProvider.MYSQL/DataProviderMYSQL.cs
+++ Snow.Web.DataProvider.MYSQL/DataProviderMYSQL.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Snow.Web.DTO;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Snow.Web.DataProvider.MYSQL
 {
@@ -10,7 +12,47 @@ namespace Snow.Web.DataProvider.MYSQL
     {
         public override CustomerList GetCustomer(string vipCode)
         {
-            return null;
+            List<Customer> customers = new List<Customer>();
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    MySqlCommand cmd = new MySqlCommand("swSP_GetCustomer", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    MySqlParameter param = new MySqlParameter("?vip_code", MySqlDbType.String);
+                    param.Direction = ParameterDirection.Input;
+                    param.Value = vipCode;
+                    cmd.Parameters.Add(param);
+
+                    cmd.Connection.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Customer customer = new Customer();
+                            customer.CustomerId = Convert.ToInt32(reader["vipcode_id"]);
+                            customer.CustomerCode = reader["vipcode"] as string;
+                            customer.FirstName = reader["vipgname"] as string;
+                            customer.LastName = reader["vipname"] as string;
+                            customer.Telephone = reader["telephone"] as string;
+                            if (reader["birthday"] != DBNull.Value)
+                                customer.BirthDate = Convert.ToDateTime(reader["birthday"]);
+
+                            customers.Add(customer);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            CustomerList customerList = new CustomerList();
+            customerList.Customers = customers;
+            customerList.TotalCount = customers.Count;
+            return customerList;
         }
     }
 }
